fdfdbbdc44c02e41432678918b35873d72f8efcf
Catherine Davis <catherinevtdavis@gmail.com>
2019-12-12 13:55:26 -0700

    added specific screen +js functionality on specificScreen button +js functionality on getAdvice button

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-function watchButtons() {
+function getAdviceButton() {
+    $('form').submit(function(event) {
+        console.log('getAdviceButton pressed')
+        event.preventDefault();
+        $('.specificScreen').toggleClass('hidden');
+        $('.loadingScreen').toggleClass('hidden');
+        }
+    )
+}
+function randomButton() {
     $('#randomButton').on('click', function() {
+        console.log('randomButton pressed');
         $('.homeScreen').toggleClass('hidden');
         $('.loadingScreen').toggleClass('hidden');
     })
+}
+function specificButton() {
+    $('#specificButton').on('click', function() {
+        console.log('specificButton pressed');
+        $('.homeScreen').toggleClass('hidden');
+        $('.specificScreen').toggleClass('hidden');
+    })
+}
+
+
+function watchButtons() {
+    randomButton();
+    specificButton();
+    getAdviceButton();
 }
 function runPage() {
     watchButtons();
