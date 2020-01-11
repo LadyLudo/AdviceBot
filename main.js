06853c4cecdc6de3aeafea72dcace6d9408ec000
Catherine Davis <catherinevtdavis@gmail.com>
2020-01-10 22:35:02 -0700

    Added background styling +added CSS animated stars +configured title +removed some css associated with past video features

--- main.js
+++ main.js
@@ -34,12 +34,17 @@ function APISpecificAdvice(searchURL) {
 function displayAudio(response) {
     console.log(response);
     $('#audio').attr("src",response.url).get(0).play();
+    $('.botImage').toggleClass("pulse animated");
+    $('#audio').on('ended', function() {
+        console.log('audio ended');
+        $('.botImage').toggleClass("pulse animated");
+    });
+
 }
 function displayRandomAdvice(responseJson) {
     console.log(responseJson);
-/*    getVideos(responseJson.slip.advice);*/
     getAudio(responseJson.slip.advice);
-    let advice = responseJson.slip.advice;
+    const advice = responseJson.slip.advice;
     $('.adviceText').text(`"${advice}"`);
     $('.loadingScreen').toggleClass('hidden');
     $('.resultsScreen').toggleClass('hidden');
@@ -47,28 +52,14 @@ function displayRandomAdvice(responseJson) {
 function displaySpecificAdvice(responseJson) {
     console.log(responseJson);
     let i = Math.round(Math.random() * responseJson.slips.length);
-  /*  getVideos(responseJson.slips[i].advice);*/
     getAudio(responseJson.slips[i].advice);
     $('.adviceText').text(`"${responseJson.slips[i].advice}"`);
     $('.loadingScreen').toggleClass('hidden');
     $('.resultsScreen').toggleClass('hidden');
 }
-function displayVideos(responseJson) {
-    console.log(responseJson);
-    for (let i=0; i<responseJson.items.length; i++) {
-        $(`#thumb${i}`).prop('src', responseJson.items[i].snippet.thumbnails.high.url);
-        $(`#title${i}`).text(decodeURIComponent(responseJson.items[i].snippet.title).replace(/&#39;/g, "'").replace(/&quot;/g, '"').replace(/&amp;/g, '&'));
-        $(`#description${i}`).text(responseJson.items[i].snippet.description)
-    }
-}
-function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${key}=${params[key]}`);
-    return queryItems.join('&');
-}
 function getAudio(adviceText) {
-    let audioAPIKey = "0f83c0855aea400a911ad0f1cc4da7fd";
-    let audioURL = `https://api.voicerss.org/?key=${audioAPIKey}&hl=en-ca&src=${adviceText}`;
+    const audioAPIKey = "0f83c0855aea400a911ad0f1cc4da7fd";
+    const audioURL = `https://api.voicerss.org/?key=${audioAPIKey}&hl=en-ca&src=${adviceText}`;
     fetch (audioURL)
         .then(response => {
             if (response.ok) {
@@ -82,34 +73,6 @@ function getAudio(adviceText) {
     });
 
 }
-/*function getVideos(adviceText) {
-    $('.videoErrorMessage').empty();
-    let url= 'https://www.googleapis.com/youtube/v3/search';
-    let searchTerm = encodeURI(adviceText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""));
-    let params = {
-        part: 'snippet',
-        key: 'AIzaSyBrC317ICJQKMjYSFMJB6KKyuztjqIQ1t4',
-        q: searchTerm,
-        maxResults: 3,
-        type: 'video',
-        order: 'Relevance',
-    };
-    let querystring = formatQueryParams(params);
-    let searchURL = url + '?' + querystring;
-    console.log(searchURL);
-    fetch (searchURL)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayVideos(responseJson));
-        /!*.catch(function displayVideoErrorMessage() {
-            $('.videoErrorMessage').text("Sorry, I was unable to find videos about that advice.");
-        }*!/
-}*/
-
 function errorRestartButton() {
     $('#errorRestartButton').on('click', function() {
         console.log('errorRestartButton pressed');
@@ -121,8 +84,8 @@ function specificAdviceButton() {
     $('form').submit(function(event) {
         console.log('getAdviceButton pressed')
         event.preventDefault();
-        let query = $('input[name=category]').val();
-        let searchURL = baseURL + '/search/' + query;
+        const query = $('input[name=category]').val();
+        const searchURL = baseURL + '/search/' + query;
         APISpecificAdvice(searchURL);
         $('.specificScreen').toggleClass('hidden');
         $('.loadingScreen').toggleClass('hidden');
